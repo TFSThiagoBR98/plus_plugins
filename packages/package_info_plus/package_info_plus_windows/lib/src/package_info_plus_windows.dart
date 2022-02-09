47db7ea404f5deccf41dd94c27fd50b5a1f9cb56
Thiago França da Silva <tfsthiagobr98@outlook.com>
2022-02-09 11:10:12 -0300

    [package_info_plus_windows] use version.json to fetch the version

--- packages/package_info_plus/package_info_plus_windows/lib/src/package_info_plus_windows.dart
+++ packages/package_info_plus/package_info_plus_windows/lib/src/package_info_plus_windows.dart
@@ -1,6 +1,7 @@
 /// The Windows implementation of `package_info_plus`.
 library package_info_plus_windows;
 
+import 'dart:convert';
 import 'dart:io';
 import 'dart:ffi';
 
@@ -8,6 +9,7 @@ import 'package:ffi/ffi.dart';
 import 'package:package_info_plus_platform_interface/package_info_data.dart';
 import 'package:package_info_plus_platform_interface/package_info_platform_interface.dart';
 import 'package:win32/win32.dart';
+import 'package:path/path.dart' as path;
 
 part 'file_version_info.dart';
 
@@ -21,18 +23,31 @@ class PackageInfoWindows extends PackageInfoPlatform {
   /// Returns a map with the following keys:
   /// appName, packageName, version, buildNumber
   @override
-  Future<PackageInfoData> getAll() {
+  Future<PackageInfoData> getAll() async {
     final info = _FileVersionInfo(Platform.resolvedExecutable);
     final versions = info.productVersion!.split('+');
+    final versionJson = await _getVersionJson();
     final data = PackageInfoData(
-      appName: info.productName ?? '',
+      appName: versionJson['app_name'] ?? (info.productName ?? ''),
       packageName: info.internalName ?? '',
-      version: versions.getOrNull(0) ?? '',
-      buildNumber: versions.getOrNull(1) ?? '',
+      version: versionJson['version'] ?? (versions.getOrNull(0) ?? ''),
+      buildNumber: versionJson['build_number'] ?? (versions.getOrNull(1) ?? ''),
       buildSignature: '',
     );
     info.dispose();
-    return Future.value(data);
+    return data;
+  }
+
+  Future<Map<String, dynamic>> _getVersionJson() async {
+    try {
+      final exePath = await File(Platform.resolvedExecutable).resolveSymbolicLinks();
+      final appPath = path.dirname(exePath);
+      final assetPath = path.join(appPath, 'data', 'flutter_assets');
+      final versionPath = path.join(assetPath, 'version.json');
+      return jsonDecode(await File(versionPath).readAsString());
+    } catch (_) {
+      return <String, dynamic>{};
+    }
   }
 }
 
